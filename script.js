29f5c82e64df63db58e6e169e6bd050c56e75ebf
luizcostaroberto-eng <luiz.costa.roberto@escola.pr.gov.br>
2026-05-27 20:42:26 -0300

    agrinho

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function verificar(resposta) {
+    const resultado = document.getElementById('resposta');
+    if (resposta === 'certa') {
+        resultado.textContent = "Parabéns! Você acertou!";
+        resultado.style.color = "green";
+    } else {
+        resultado.textContent = "Ops! Essa não é sustentável. Tente novamente!";
+        resultado.style.color = "red";
+    }
+}
